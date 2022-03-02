f962f0f6a4df8c45c5e36eb509b5bed85419c133
Nicolas Jaimes <stiven77.nj@gmail.com>
2022-03-01 21:47:45 -0500

    Agregando estructura y estilos a la pagina home

--- danic/ventas/views.py
+++ danic/ventas/views.py
@@ -5,13 +5,20 @@ from django.template import loader
 
 # Create your views here.
 
-# Vista para el inicio de la aplicacion
+# Vista de login de la aplicacion
 def login(request):
     template = loader.get_template('login/login.html')
     context = {}
     return HttpResponse(template.render(context,request))
 
+# Vista del registro de la aplicacion 
 def registro(request):
     template = loader.get_template('login/registro.html')
+    context = {}
+    return HttpResponse(template.render(context,request))
+
+# Vista del home de la aplicacion
+def home(request):
+    template = loader.get_template('productos/home.html')
     context = {}
     return HttpResponse(template.render(context,request))
